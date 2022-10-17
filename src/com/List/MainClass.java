0c75337419fa02ef6c7e76c10df561e2cb12459e
Abhishek Pal <palabhi609@gmail.com>
2022-10-17 17:50:22 +0530

    adding changes

--- src/com/List/MainClass.java
+++ src/com/List/MainClass.java
@@ -5,19 +5,18 @@ import java.util.List;
 
 public class MainClass {
 	public static void main(String[] args) {
-Student s1=new Student(11, "Abhishek", 90);
-Student s2=new Student(12, "Abhi", 80);
-Student s3=new Student(13, "Gourav", 95);
-Student s4=new Student(14, "Deep", 88);
-Student s5=new Student(15, "Jatin", 99);
-List<Student> list=new ArrayList<>();
-list.add(s1);
-list.add(s2);
-list.add(s3);
-list.add(s4);
-list.add(s5);
-System.out.println(list);
-System.out.println("this is a main class");
-System.out.println("class");
+		Student s1 = new Student(11, "Abhishek", 90);
+		Student s2 = new Student(12, "Abhi", 80);
+		Student s3 = new Student(13, "Gourav", 95);
+		Student s4 = new Student(14, "Deep", 88);
+		Student s5 = new Student(15, "Jatin", 99);
+		List<Student> list = new ArrayList<>();
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
+		list.add(s4);
+		list.add(s5);
+		System.out.println(list);
+		System.out.println("this is a main class");
 	}
 }
