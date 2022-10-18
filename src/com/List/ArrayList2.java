b831f53903ee4381e169aef8c6b1697b60e36827
Abhishek Pal <palabhi609@gmail.com>
2022-10-18 14:19:04 +0530

    add changes

--- src/com/List/ArrayList2.java
+++ src/com/List/ArrayList2.java
@@ -15,7 +15,13 @@ public class ArrayList2 {
 		list.add(9, "Virat");
 		
 		System.out.println(list);
+<<<<<<< HEAD
 		System.out.println("i like coding");
+=======
+		
+		//Hey its me, Swasti!
+		//commit new changes
+>>>>>>> 36cf716e2bf2a2701e65e82305dec38654b2fac5
 	}
 
 }
